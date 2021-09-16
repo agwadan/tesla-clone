ba52f0f02004ec072e914c80f597576a4af99bcc
agwadan <agwa.dan@gmail.com>
2021-09-16 15:58:44 +0300

    Add and style buttons

--- src/components/section.jsx
+++ src/components/section.jsx
@@ -8,6 +8,13 @@ const Section = () => {
         <h1>Model S</h1>
         <p>Order Online for Touchless Delivery </p>
       </ItemText>
+      <div>
+        <ButtonGroup>
+          <LeftButton>Custom Order</LeftButton>
+          <RightButton>Existing Intentory</RightButton>
+        </ButtonGroup>
+        <DownArrow src='./assets/images/down-arrow.svg' />
+      </div>
     </Wrap>
   )
 }
@@ -20,9 +27,42 @@ const Wrap = styled.div`
   background-position: center;
   background-repeat: no-repeat;
   background-image: url('../assets/images/model-s.jpg');
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `
-
 const ItemText = styled.div`
   padding-top: 15vh;
   text-align: center;
+`
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  /* flex-direction: column; */
+`
+const LeftButton = styled.div`
+  background-color: rgba(23,26,32,0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`
+const RightButton = styled(LeftButton)`
+  background: white;
+  opacity: 0.65;
+  color: black;
+`
+const DownArrow = styled.img`
+  height: 40px;
+  cursor: pointer;
+  animation: animateDown infinite 1.5s;
 `
